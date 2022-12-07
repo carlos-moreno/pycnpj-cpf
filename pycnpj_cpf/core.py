5c451011334c449604a6c813d1d7de74d2026509
Carlos Moreno <omorenodovale@gmail.com>
2022-12-07 10:09:46 -0300

    Added news unit tests (#11)
    * Added news unit tests
    
    * Remove import not used
    
    closes #8

--- pycnpj_cpf/core.py
+++ pycnpj_cpf/core.py
@@ -20,107 +20,78 @@ def remove_punctuation(value: str) -> str:
     return sub(PATTERN, "", value)
 
 
-def extract_digits(value: str) -> str:
-    return "".join(list(filter(lambda x: x.isdigit(), value)))
-
-
-def check_value_cpf(value: str) -> bool:
-    value = remove_punctuation(value)
-    return value.isdigit() and len(value) == SIZE_CPF
-
-
-def check_value_cnpj(value: str) -> bool:
-    value = remove_punctuation(value)
-    return value.isdigit() and len(value) == SIZE_CNPJ
-
-
 def first_digit_cpf_checker_is_valid(value: str) -> bool:
     value = remove_punctuation(value)
-    if check_value_cpf(value):
-        combinations = list(
-            zip(CPF.get("first_digit"), [int(x) for x in value[:10]])
-        )
-        sum_ = 0
-        for item in combinations:
-            x, y = item
-            sum_ += x * y
-        rest_of_division = sum_ % NUMBER_FOR_REST
-        result = (
-            0 if rest_of_division < 2 else NUMBER_FOR_REST - rest_of_division
-        )
-        return result == int(value[9])
-    else:
-        return False
+    combinations = list(
+        zip(CPF.get("first_digit"), [int(x) for x in value[:10]])
+    )
+    sum_ = 0
+    for item in combinations:
+        x, y = item
+        sum_ += x * y
+    rest_of_division = sum_ % NUMBER_FOR_REST
+    result = 0 if rest_of_division < 2 else NUMBER_FOR_REST - rest_of_division
+    return result == int(value[9])
 
 
 def second_digit_cpf_checker_is_valid(value: str) -> bool:
     value = remove_punctuation(value)
-    if check_value_cpf(value):
-        combinations = list(
-            zip(CPF.get("second_digit"), [int(x) for x in value[:11]])
-        )
-        sum_ = 0
-        for item in combinations:
-            x, y = item
-            sum_ += x * y
-        rest_of_division = sum_ % NUMBER_FOR_REST
-        result = (
-            0 if rest_of_division < 2 else NUMBER_FOR_REST - rest_of_division
-        )
-        return result == int(value[10])
-    else:
-        return False
+    combinations = list(
+        zip(CPF.get("second_digit"), [int(x) for x in value[:11]])
+    )
+    sum_ = 0
+    for item in combinations:
+        x, y = item
+        sum_ += x * y
+    rest_of_division = sum_ % NUMBER_FOR_REST
+    result = 0 if rest_of_division < 2 else NUMBER_FOR_REST - rest_of_division
+    return result == int(value[10])
 
 
 def first_digit_cnpj_checker_is_valid(value: str) -> bool:
     value = remove_punctuation(value)
-    if check_value_cnpj(value):
-        combinations = list(
-            zip(CNPJ.get("first_digit"), [int(x) for x in value[:12]])
-        )
-        sum_ = 0
-        for item in combinations:
-            x, y = item
-            sum_ += x * y
-        rest_of_division = sum_ % NUMBER_FOR_REST
-        result = (
-            0 if rest_of_division < 2 else NUMBER_FOR_REST - rest_of_division
-        )
-        return result == int(value[12])
-    else:
-        False
+    combinations = list(
+        zip(CNPJ.get("first_digit"), [int(x) for x in value[:12]])
+    )
+    sum_ = 0
+    for item in combinations:
+        x, y = item
+        sum_ += x * y
+    rest_of_division = sum_ % NUMBER_FOR_REST
+    result = 0 if rest_of_division < 2 else NUMBER_FOR_REST - rest_of_division
+    return result == int(value[12])
 
 
 def second_digit_cnpj_checker_is_valid(value: str) -> bool:
     value = remove_punctuation(value)
-    if check_value_cnpj(value):
-        combinations = list(
-            zip(CNPJ.get("second_digit"), [int(x) for x in value[:13]])
-        )
-        sum_ = 0
-        for item in combinations:
-            x, y = item
-            sum_ += x * y
-        rest_of_division = sum_ % NUMBER_FOR_REST
-        result = (
-            0 if rest_of_division < 2 else NUMBER_FOR_REST - rest_of_division
-        )
-        return result == int(value[13])
-    else:
-        False
+    combinations = list(
+        zip(CNPJ.get("second_digit"), [int(x) for x in value[:13]])
+    )
+    sum_ = 0
+    for item in combinations:
+        x, y = item
+        sum_ += x * y
+    rest_of_division = sum_ % NUMBER_FOR_REST
+    result = 0 if rest_of_division < 2 else NUMBER_FOR_REST - rest_of_division
+    return result == int(value[13])
 
 
 def cnpj_or_cpf_is_valid(value: str) -> bool:
-    value = remove_punctuation(value)
     result = False
-    if len(value) == SIZE_CPF and (
-        first_digit_cpf_checker_is_valid(value)
-        and second_digit_cpf_checker_is_valid(value)
-    ):
-        result = True
-    if len(value) == SIZE_CNPJ and (
-        first_digit_cnpj_checker_is_valid(value)
-        and second_digit_cnpj_checker_is_valid(value)
-    ):
-        result = True
+
+    try:
+        value = remove_punctuation(value)
+
+        if len(value) == SIZE_CPF and (
+            first_digit_cpf_checker_is_valid(value)
+            and second_digit_cpf_checker_is_valid(value)
+        ):
+            result = True
+        if len(value) == SIZE_CNPJ and (
+            first_digit_cnpj_checker_is_valid(value)
+            and second_digit_cnpj_checker_is_valid(value)
+        ):
+            result = True
+    except (IndexError, ValueError):
+        ...
     return result
